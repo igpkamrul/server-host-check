9f1b0f435d843e33e00f250be5aa5425d51e76eb
igpkamrul <kamrulhaider.igp@gmail.com>
2022-08-11 13:42:13 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is for checking hosting and domain pointing in vps server</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
